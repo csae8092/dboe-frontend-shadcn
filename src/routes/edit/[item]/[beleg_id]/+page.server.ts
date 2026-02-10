844e69e73411800f0d821f7a2fd4dbb54652d18d
csae8092 <p.andorfer@gmail.com>
2026-02-10 12:34:37 +0100

    feat: edit for related objects

---
+++ src/routes/edit/[item]/[beleg_id]/+page.server.ts
@@ -0,0 +1,29 @@
+import { redirect } from '@sveltejs/kit';
+import type { Actions } from './$types';
+import { routeMapper } from '$lib/constants';
+import { getJwt } from '$lib/server/auth';
+import { fieldsToExclude } from '$lib/constants';
+
+export const actions: Actions = {
+	default: async ({ request, url, params, cookies }) => {
+		const formData = await request.formData();
+        console.log(formData)
+		const payload = Object.fromEntries(formData.entries());
+		const jwt = getJwt(cookies);
+		const api_url = routeMapper[params.item as keyof typeof routeMapper].api_url;
+		console.log(`${api_url}${payload.id}`);
+
+		const response = await fetch(`${api_url}${payload.id}/`, {
+			method: 'PATCH',
+			headers: {
+				accept: 'application/json',
+				'Content-Type': 'application/json',
+				Authorization: `Token ${jwt}`
+			},
+			body: JSON.stringify(payload)
+		});
+        console.log(response)
+		const referer = request.headers.get('referer') || '/';
+		throw redirect(303, referer);
+	}
+};
--- src/routes/edit/[item]/[beleg_id]/+page.svelte
+++ src/routes/edit/[item]/[beleg_id]/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import { fieldsToExclude } from '$lib/constants.js';
 	import * as Table from '$lib/components/ui/table/index.js';
+	import * as Dialog from '$lib/components/ui/dialog/index.js';
+	import { Button, buttonVariants } from '$lib/components/ui/button/index.js';
+	import Label from '$lib/components/ui/label/label.svelte';
+	import Input from '$lib/components/ui/input/input.svelte';
 	const { data, params } = $props();
 </script>
 
@@ -17,6 +21,7 @@
 		</Table.Caption>
 		<Table.Header>
 			<Table.Row>
+				<Table.Head class="text-center">edit</Table.Head>
 				{#each Object.keys(data.payload.results[0]) as x, i}
 					{#if !fieldsToExclude.includes(x)}
 						<Table.Head id={`th-id-${i}`}>{x}</Table.Head>
@@ -27,9 +32,47 @@
 		<Table.Body>
 			{#each data.payload.results as row, i}
 				<Table.Row id={`tr-id-${i}`}>
+					<Table.Cell>
+						<Dialog.Root>
+							<Dialog.Trigger type="button" class={buttonVariants({ variant: 'outline' })}
+								>Edit Item</Dialog.Trigger
+							>
+							<Dialog.Content>
+								<form method="POST">
+									<Dialog.Header>
+										<Dialog.Title>Edit {params.item}: ID {row.id}</Dialog.Title>
+										<Dialog.Description>Modify the object and click submit</Dialog.Description>
+									</Dialog.Header>
+									<div class="grid gap-4">
+										{#each Object.entries(row) as [key, value], fid}
+											{#if !fieldsToExclude.includes(key)}
+												{#if key === 'id' || key === 'beleg'}
+													<Input type="hidden" id={`${key}-${fid}`} name={key} {value} />
+												{:else}
+													<div class="grid gap-3" id={`modify-field-${fid}`}>
+														<Label for={`${key}-${fid}`}>{key}</Label>
+														<Input id={`${key}-${fid}`} name={key} {value} />
+													</div>
+												{/if}
+											{/if}
+										{/each}
+									</div>
+									<Dialog.Footer>
+										<Dialog.Close class={buttonVariants({ variant: 'outline' })}
+											>Cancel</Dialog.Close
+										>
+										<Button type="submit">Save changes</Button>
+									</Dialog.Footer>
+								</form>
+							</Dialog.Content>
+						</Dialog.Root>
+					</Table.Cell>
 					{#each Object.entries(row) as [key, value], tdid}
 						{#if key === 'id'}
-							<Table.Cell id={`td-id-${tdid}`}><a class="underline decoration-dotted" href={`${row.url}?format=json`}>{value}</a></Table.Cell>
+							<Table.Cell id={`td-id-${tdid}`}
+								><a class="underline decoration-dotted" href={`${row.url}?format=json`}>{value}</a
+								></Table.Cell
+							>
 						{:else if !fieldsToExclude.includes(key)}
 							<Table.Cell id={`td-id-${tdid}`}>{value}</Table.Cell>
 						{/if}
